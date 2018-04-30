2c329577e4f551febbcbc87aff333347997fde30
Onyekachi Okereke <robocopkaka@gmail.com>
2018-04-30 18:56:05 +0100

    Working simple todo app with meteor

---
+++ imports/ui/App.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { withTracker } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
+import Task from './Task.js';
+
+// App component - represents the whole app
+class App extends Component {
+  // getTasks() {
+  //   return [
+  //     { _id: 1, text: 'This is task 1' },
+  //     { _id: 2, text: 'This is task 2' },
+  //     { _id: 3, text: 'This is task 3' },
+  //   ];
+  // }
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date()
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
+  renderTasks() {
+    return this.props.tasks.map((task) => (
+      <Task key={task._id} task={task} />
+    ));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
+        </header>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+}
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+  }
+})(App);
