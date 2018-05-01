365b869f21dec8fabb42c6225b0557a678e66840
Okereke Onyekachi <robocopkaka@gmail.com>
2018-05-01 22:58:49 +0100

    Hide incomplete tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,13 +7,13 @@ import Task from './Task.js';
 
 // App component - represents the whole app
 class App extends Component {
-  // getTasks() {
-  //   return [
-  //     { _id: 1, text: 'This is task 1' },
-  //     { _id: 2, text: 'This is task 2' },
-  //     { _id: 3, text: 'This is task 3' },
-  //   ];
-  // }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false
+    };
+  }
   handleSubmit(e) {
     e.preventDefault();
 
@@ -27,8 +27,18 @@ class App extends Component {
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    })
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -37,7 +47,17 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List {this.props.incompleteCount}</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
             <input
@@ -58,5 +78,6 @@ class App extends Component {
 export default withTracker(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   }
 })(App);
